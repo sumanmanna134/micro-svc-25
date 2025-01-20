5ddb0fe8d12854df2f7cbf28a41021146acfe9d2
sumanmanna134 <manna.suman134@gmail.com>
2025-01-21 00:42:25 +0530

    add copyrights

--- catalog-svc/src/main/java/com/example/catalog/CatalogSvcApplication.java
+++ catalog-svc/src/main/java/com/example/catalog/CatalogSvcApplication.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2025  IsumDev. All rights reserved.
+ *
+ * This software, including its code and documentation, is the proprietary property of IsumDev
+ * and is protected by applicable copyright laws. Unauthorized use, reproduction, modification, or distribution is strictly prohibited.
+ * By using this software, you agree to abide by these terms.
+ *  For licensing or inquiries, contact IsumDev. All rights are reserved under international and domestic intellectual property laws.
+ */
+
 package com.example.catalog;
 
 import org.springframework.boot.SpringApplication;
